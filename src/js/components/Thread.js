b974aff0dfc5d7419ffa175382c72641730b29f7
Frank Adelardi <fmadelardi@gmail.com>
2016-08-23 23:34:24 +0200

    Fixing routes

--- src/js/components/Thread.js
+++ src/js/components/Thread.js
@@ -5,9 +5,9 @@ var Thread = React.createClass({
   render: function() {
     return (
       <li>
-        <div><Link to="/thread">{this.props.title}</Link></div>
-        <div>submitted {this.props.timestring} by <Link to={"/user/" + encodeURIComponent(this.props.author)}>{this.props.author}</Link> to <Link to="/">{this.props.subforum}</Link></div>
-        <div><Link to={"/thread/" + encodeURIComponent(this.props.title)}>{this.props.totalComments} comment(s)</Link></div>
+        <div><Link to={"/t/" + encodeURIComponent(this.props.title)}>{this.props.title}</Link></div>
+        <div>submitted {this.props.timestring} by <Link to={"/u/" + encodeURIComponent(this.props.author)}>{this.props.author}</Link> to <Link to="/">{this.props.subforum}</Link></div>
+        <div><Link to={"/t/" + encodeURIComponent(this.props.title)}>{this.props.totalComments} comment(s)</Link></div>
       </li>
     );
   }
