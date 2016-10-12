a879906ced335be89a28989ae262843d24d4de69
fadelardi <fmadelardi@gmail.com>
2016-10-12 20:20:31 +0200

    Adding tests for Header component

---
+++ test/components/Header.test.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var enzyme = require('enzyme');
+var expect = require('chai').expect;
+var shallow = enzyme.shallow;
+var mount = enzyme.mount;
+var render = enzyme.render;
+
+var Header = require('../../src/js/components/Header.js');
+var UserBox = require('../../src/js/components/UserBox');
+var Link = require('react-router').Link;
+
+
+describe('Header component', function() {
+  var wrapper = render(<Header forum="1" />);
+
+  it('contains an index link', function() {
+    expect(wrapper.text()).to.contain('Index');
+  });
+
+  it('contains a create thread link', function() {
+    expect(wrapper.text()).to.contain('Create Thread');
+  });
+
+  it('contains an UserBox component', function() {
+    var wrapper = shallow(<Header forum="1" />);
+    expect(wrapper.find(UserBox).length).to.equal(1);
+  });
+});
