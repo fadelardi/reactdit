4ebeb14cc2014ab4e897a668f57587d4d6bf15bc
fadelardi <fmadelardi@gmail.com>
2016-10-25 23:39:54 +0200

    Missing unit test comment actions, fixing some formatting

--- test/actions/commentActions.test.js
+++ test/actions/commentActions.test.js
@@ -5,7 +5,7 @@ var mockStore = configureMockStore([promise(), thunk]);
 var moxios = require('moxios');
 var expect = require('chai').expect;
 
-var commentActions = require('../../src/js/actions/commentActions.js');
+var commentActions = require('../../src/js/actions/commentActions');
 
 describe('comment actions', function() {
   beforeEach(function() {
@@ -16,35 +16,55 @@ describe('comment actions', function() {
     moxios.uninstall();
   });
 
+
   describe('addComment', function() {
-      var store = mockStore({threads: []});
+    var store = mockStore({threads: []});
+    it('will get a pending and fulfilled response, then fetch the thread', function() {
+      var mockResponse = [
+        {type: 'ADD_COMMENT_PENDING'},
+        {type: 'ADD_COMMENT_FULFILLED', payload: {data: []}},
+        {type: 'FETCH_THREAD_PENDING'}
+      ];
 
-      it('will get a pending and fulfilled response, then fetch the thread', function() {
-        var mockResponse = [
-          {type: 'ADD_COMMENT_PENDING'},
-          {type: 'ADD_COMMENT_FULFILLED', payload: {data: []}},
-          {type: 'FETCH_THREAD_PENDING'}
-        ];
-
-        moxios.wait(function() {
-          var request = moxios.requests.mostRecent();
-          request.respondWith({
-            status: 200,
-            response: []
-          });
-        });
-
-        return store.dispatch(commentActions.addComment({id: 1})).then(function() {
-          expect(store.getActions()[0].type).to.equal(mockResponse[0].type);
-          expect(store.getActions()[1].type).to.equal(mockResponse[1].type);
-          expect(store.getActions()[1].payload.data).to.eql(mockResponse[1].payload.data);
-          expect(store.getActions()[2].type).to.equal(mockResponse[2].type);
-
+      moxios.wait(function() {
+        var request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: []
         });
       });
-    });
 
-    describe('getComments', function() {
-      //TODO
+      return store.dispatch(commentActions.addComment({id: 1})).then(function() {
+        expect(store.getActions()[0].type).to.equal(mockResponse[0].type);
+        expect(store.getActions()[1].type).to.equal(mockResponse[1].type);
+        expect(store.getActions()[1].payload.data).to.eql(mockResponse[1].payload.data);
+        expect(store.getActions()[2].type).to.equal(mockResponse[2].type);
+      });
     });
+  });
+
+  describe('getComments', function() {
+    var store = mockStore({threads: []});
+
+    it('will get a pending and fulfilled response', function() {
+      var mockResponse = [
+        {type: 'FETCH_COMMENTS_PENDING'},
+        {type: 'FETCH_COMMENTS_FULFILLED', payload: { data: [] }}
+      ];
+
+      moxios.wait(function() {
+        var request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: []
+        });
+      });
+
+      return store.dispatch(commentActions.getComments(1)).then(function() {
+        expect(store.getActions()[0].type).to.equal(mockResponse[0].type);
+        expect(store.getActions()[1].type).to.equal(mockResponse[1].type);
+        expect(store.getActions()[1].payload.data).to.eql(mockResponse[1].payload.data);
+      });
+    });
+  });
 });
