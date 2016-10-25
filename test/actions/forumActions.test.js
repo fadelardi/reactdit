a26984d296ea5ee96249589361f30f38276ee16b
fadelardi <fmadelardi@gmail.com>
2016-10-25 23:49:20 +0200

    Test for getForums

--- test/actions/forumActions.test.js
+++ test/actions/forumActions.test.js
@@ -17,6 +17,26 @@ describe('forum actions', function() {
   });
 
   describe('getForums', function() {
-    //TODO
+    var store = mockStore({threads: []});
+    it('will get a pending and fulfilled response', function() {
+      var mockResponse = [
+        {type: 'FETCH_FORUMS_PENDING'},
+        {type: 'FETCH_FORUMS_FULFILLED', payload: { data: [] }}
+      ];
+
+      moxios.wait(function() {
+        var request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: []
+        });
+      });
+
+      return store.dispatch(forumActions.getForums()).then(function() {
+        expect(store.getActions()[0].type).to.equal(mockResponse[0].type);
+        expect(store.getActions()[1].type).to.equal(mockResponse[1].type);
+        expect(store.getActions()[1].payload.data).to.eql(mockResponse[1].payload.data);
+      });
+    });
   });
 });
