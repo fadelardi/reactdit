927e6bea294e8a2dddc0032e3b5329030b1b1a66
fadelardi <fmadelardi@gmail.com>
2016-10-27 15:20:34 +0200

    Formatting date displayed in thread

--- src/js/components/Thread.js
+++ src/js/components/Thread.js
@@ -1,12 +1,18 @@
 var React = require('react');
 var Link = require('react-router').Link;
+var moment = require('moment');
 
 var Thread = React.createClass({
+  formatDate: function(date) {
+    var dateString = moment().from(date, true);
+    return (dateString != 'Invalid date') ? dateString + ' ago' : 'unknown time ago';
+  },
+
   render: function() {
     return (
       <li>
         <div><Link to={"/t/" + this.props.id}>{this.props.title}</Link></div>
-        <div>submitted {this.props.timestring} by <Link to={"/u/" + encodeURIComponent(this.props.author)}>{this.props.author}</Link> to <Link to={'/f/' + encodeURIComponent(this.props.forum)}>{this.props.forum}</Link></div>
+        <div>submitted {this.formatDate(this.props.timestring)} by <Link to={"/u/" + encodeURIComponent(this.props.author)}>{this.props.author}</Link> to <Link to={'/f/' + encodeURIComponent(this.props.forum)}>{this.props.forum}</Link></div>
         <div><Link to={"/t/" + this.props.id}>{this.props.totalComments} comment(s)</Link></div>
       </li>
     );
