4031bd13d82b429021d3d286427750b6d179ccf7
fadelardi <fmadelardi@gmail.com>
2016-10-31 00:08:02 +0100

    Adding types

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   API_URL: 'http://localhost:3000',
-  ANON_ID: 0
+  ANON_ID: 0,
+  THREAD_TYPES: [
+    { id: 'TXT', desc: 'Text' },
+    { id: 'IMG', desc: 'Image' },
+    { id: 'LNK', desc: 'Link' }
+  ]
 };
