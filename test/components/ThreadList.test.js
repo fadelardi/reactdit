bf3bca4470ce7dcbd65f4ff67ef7937d21ca4124
Frank Adelardi <francisco.adelardi@aeriagames.com>
2016-11-03 15:35:05 +0100

    id prop is used as key, and needs to be present

--- test/components/ThreadList.test.js
+++ test/components/ThreadList.test.js
@@ -7,7 +7,7 @@ var ThreadList = require('../../src/js/components/ThreadList');
 var Thread = require('../../src/js/components/Thread');
 
 describe('<ThreadList />', function() {
-  var threads = [{}, {}];
+  var threads = [{id:1}, {id:2}];
   var wrapper = shallow(<ThreadList threads={threads} />);
 
   it('should find two Thread components', function() {
