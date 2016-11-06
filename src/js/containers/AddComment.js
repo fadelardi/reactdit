a0ab3914f030e0e7fd96836132a5ff658a003a74
fadelardi <fmadelardi@gmail.com>
2016-11-06 17:03:36 +0100

    Empty comment validation

--- src/js/containers/AddComment.js
+++ src/js/containers/AddComment.js
@@ -1,3 +1,4 @@
+var ANON_ID = require('../config').ANON_ID;
 var React = require('react');
 var connect = require('react-redux').connect;
 var commentActions = require('../actions/commentActions');
@@ -26,26 +27,31 @@ var AddComment = React.createClass({
     this.setState({text: e.value});
   },
 
-  handleSubmit: function() {
-    var data, commentValue = this.refs.comment.value;
-    if (commentValue != '') {
-      data = {
-        id: this.props.threadId,
-        parent_id: (typeof this.props.commentId == 'undefined') ? 0 : this.props.commentId,
-        uid: 1,
-        comment: this.refs.comment.value
-      };
+  handleSubmit: function(e) {
+    var comment = e.target.comment.value;
 
-      this.props.dispatch(commentActions.addComment(data));
+    e.preventDefault();
+
+    if (comment == '' || typeof comment == 'undefined') {
+      return;
     }
+
+    this.props.dispatch(commentActions.addComment({
+      id: this.props.threadId,
+      parent_id: (typeof this.props.commentId == 'undefined') ? 0 : this.props.commentId,
+      uid: ANON_ID,
+      comment: comment
+    }));
   },
 
   render: function() {
     return (
       <div className="addComment col-md-12">
+        <form onSubmit={this.handleSubmit}>
         <div>Add your comment:</div>
-        <textarea ref="comment" onChange={this.handleChange} value={this.state.text} />
-        <div><button onClick={this.handleSubmit}>Submit Post</button></div>
+        <textarea name="comment" onChange={this.handleChange} value={this.state.text} required />
+        <div><input type="submit" value="Submit post" /></div>
+        </form>
       </div>
     );
   }
